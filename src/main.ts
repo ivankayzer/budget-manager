c5d15ce3a5834385b7a91c78507b610718d9673b
Ivan Kayzer <ivan.kayzer@outlook.com>
2021-07-24 09:55:09 +0200

    feat: add headers

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: "'none'",
+        },
+      },
+      frameguard: {
+        action: 'deny',
+      },
+    }),
+  );
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
 
   await app.listen(process.env.PORT || 3000);
 }
